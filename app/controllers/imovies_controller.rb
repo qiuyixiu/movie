2b9c59024a0f4af1cd55635fefa464e3ee6b1ac8
ken <aken@matoMac-mini-3.local>
2017-03-23 17:43:25 +0800

    implement imovies#new

--- app/controllers/imovies_controller.rb
+++ app/controllers/imovies_controller.rb
@@ -2,4 +2,8 @@ class ImoviesController < ApplicationController
   def index
     @imovies = Imovie.all
   end
+
+  def new
+    @imovie = Imovie.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :imovies
+  root 'imovies#index'
 end
