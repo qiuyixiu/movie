63081d9120c0576b6a8c83fcc919596304c8049c
ken <aken@matoMac-mini-3.local>
2017-03-23 18:05:31 +0800

    implement cr

--- app/controllers/imovies_controller.rb
+++ app/controllers/imovies_controller.rb
@@ -3,7 +3,24 @@ class ImoviesController < ApplicationController
     @imovies = Imovie.all
   end
 
+  def show
+    @imovie = Imovie.find(params[:id])
+  end
+
   def new
     @imovie = Imovie.new
   end
+
+  def create
+    @imovie = Imovie.new(imovie_params)
+    @imovie.save
+       redirect_to imovies_path
+  end
+
+  private
+
+  def imovie_params
+    params.require(:imovie).permit(:电影名称, :简介)
+  end
+
 end
